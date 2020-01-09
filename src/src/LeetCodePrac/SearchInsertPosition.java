8441c465b08774156d62d2944a997ce70b03f38d
ceiling12138 <ceiling12138@aliyun.com>
2020-01-09 15:16:09 +0800

    wrong answer

--- src/src/LeetCodePrac/SearchInsertPosition.java
+++ src/src/LeetCodePrac/SearchInsertPosition.java
@@ -14,8 +14,35 @@ public class SearchInsertPosition {
             }
             return index;
     }
+    //折半查找  本题不适用!!!!错了！！！！
+    static int solution(int[] nums, int target){
+        int low = 1, high = nums.length;
+        int mid=0;
+        if (nums[low-1] >= target){
+            return low-1;
+        }
+        if (nums[high-1] < target){
+            return high;
+        }else if (nums[high-1] == target){
+            return high-1;
+        }
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] == target) {return mid;}
+            else if (nums[mid] > target){
+                high = mid -1;
+            }else {
+                low = mid + 1;
+            }
+            System.out.println("mid = " + mid);
+        }
+        return mid;
+    }
+
+
     public static void main(String[] args){
-        int[] n =  {1};
-        System.out.println(searchInsert(n,0));
+        int[] n =  {1,3,5};
+//        System.out.println(searchInsert(n,0));
+        System.out.println(solution(n,4));
     }
 }
